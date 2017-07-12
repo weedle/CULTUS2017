c604de59653bdf9453c96ac190fbd106895b56a0
Kevin Jayamanna <mercury0110111@gmail.com>
2017-07-11 17:26:53 -0700

    fixed merge issue

--- cultus-trial/Assets/scripts/Menus/Icons/ActionIcon.cs
+++ cultus-trial/Assets/scripts/Menus/Icons/ActionIcon.cs
@@ -1,18 +1,36 @@
 ﻿using UnityEngine;
-using System.Collections;
-using System;
 using System.Collections.Generic;
-using UnityEngine.UI;
+using System;
 
-public class ActionIcon : MonoBehaviour, IntfMenuItem {
+public class ActionIcon : MonoBehaviour, IntfMenuItem
+{
+    private ActionMenu menu;
+    public void activate()
+    {
+        if (menu != null)
+            return;
+        Unit unit = GameObject.Find("GameLogic").GetComponent<TurnHandler>().getCurrentUnit();
+        List<String> allActions = unit.getAvailableActions();
 
-	public void activate() {
-		Unit unit = GameObject.Find ("GameLogic").GetComponent<TurnHandler> ().getCurrentUnit ();
-		List<String> allActions = unit.getAvailableActions ();
-	}
+        menu = new GameObject("actionMenu")
+            .AddComponent<ActionMenu>();
+        menu.gameObject.transform.parent = GameObject.Find("canvas").transform;
+        menu.transform.localScale = new Vector3(0.75f, 0.75f, 1);
+        foreach (String action in allActions)
+        {
+            menu.addItem(action);
+        }
+    }
 
-	public void OnMouseDown() {
-		activate();
-	}
+    public void deactivate()
+    {
+        if (menu != null)
+            menu.destroyMenu();
+    }
+
+    public void OnMouseDown()
+    {
+        activate();
+    }
 
 }
