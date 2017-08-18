b3e7d2dcdece090a22512fd7268b426ef7833fff
Jenne H <iv.castaire@gmail.com>
2017-08-17 21:47:19 -0700

    convenience commit 1

--- cultus-trial/Assets/Scripts/UI/Victuuri.cs
+++ cultus-trial/Assets/Scripts/UI/Victuuri.cs
@@ -2,16 +2,29 @@
 using System.Collections;
 
 
-// USAGE: generates some simple animations for the 'victuuri' scene + potential miscellaneousness
+// USAGE: generates the shitty effects you see on the 'Victuuri' screen
 public class Victuuri : MonoBehaviour {
 
+	private string victoryText = "VICTUUUUUURI. >W<";
+
+	public Color color1 = new Color(46,0,255); // very blue
+	public Color color2 = new Color(255,0,47); // hot fucking pink
 
 	void Start () {
-	
+		
 	}
-
-
+		
 	void Update () {
-	
+
 	}
+
+	// USAGE: flickers the shadow behind the text in a pleasing manner
+	void neonFlicker(GameObject text) {
+
+
+	}
+
+
+
+
 }
